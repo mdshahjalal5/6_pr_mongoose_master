18b86c8d3049e62471b1123ebc066279d7f33d50
mdshahjalal2 <muhommodshahjalal@gmail.com>
2023-05-17 08:12:44 +0600

    part 7 has done

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
+// ! cojur => cors> json>urlEconded>route
+
 import { Application, Request, Response } from "express";
 import express from "express";
-import userRoute from './app/modules/user.route'
+import userRoute from './app/modules/user/user.route'
+
 const app:Application = express();
 app.use(express.json())
 app.use('/chk', userRoute)
