a6667c6d874dce473fa557a7dd54242c14df50a0
mdshahjalal2 <muhommodshahjalal@gmail.com>
2023-05-17 08:15:29 +0600

    part 7 has done

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,10 @@
 import { Application, Request, Response } from "express";
 import express from "express";
 import userRoute from './app/modules/user/user.route'
+import cors from "cors";
 
 const app:Application = express();
+app.use()
 app.use(express.json())
 app.use('/chk', userRoute)
 app.get("/", (req:Request, res:Response) => {
