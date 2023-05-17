02ee255ee86f46b3a5cb9a2456e5e024838cb052
mdshahjalal2 <muhommodshahjalal@gmail.com>
2023-05-17 08:43:38 +0600

    part 7 has done

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,16 @@
+// ! rup => router> pogud> post> get> update> delete 
 import { NextFunction, Request, Response } from "express";
 import express from "express";
 import { get_user } from "./user.controller";
 
+
+
+// !router 
 const router = express.Router();
 
+
+
+// !post get update delete 
 router.get("/", get_user  );
 
 export default router;
